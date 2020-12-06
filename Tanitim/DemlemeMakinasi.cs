09e2ae9806b856853aed03ffe12ea4720fe206ee
NZiya <arslanziya@yahoo.com>
2020-12-06 16:09:47 +0300

    Add project files.

---
+++ Tanitim/DemlemeMakinasi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tanitim
+{
+    public class DemlemeMakinasi
+    {
+        public int SuKapasitesi { get; set; }
+        public string Renk { get; set; }
+        public string Marka { get; set; }
+    }
+}
